2a133a6c20d8522e73e3472325e5c010592b2449
SolianaYemane <155404423+SolianaYemane@users.noreply.github.com>
2024-01-04 19:32:39 -0800

    Update DemoB.java

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -14,5 +14,10 @@ public class DemoB {
    public static void add(int a,int b, int c) {
 	System.out.println(a+b+c);  
 }  
+	 //coming from getHub
+   public static void subtract(int a,int b) {
+	System.out.println(a-b);  
+}  
+	
 } 
   
